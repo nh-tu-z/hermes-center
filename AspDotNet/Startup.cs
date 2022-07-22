2d200dfd3ffa3854ef76732b42a4d612b95312d4
Tu Ngo <nhtu1611@gmail.com>
2022-07-22 12:24:09 +0700

    init hermes-center

---
+++ AspDotNet/Startup.cs
@@ -0,0 +1,56 @@
+﻿namespace MyFirstCoreApp
+{
+    public class Startup
+    {
+        #region App Config Variables
+
+        protected const string _version = "v1";
+        protected const string _appName = "Cranium Security Center Admin API";
+        protected const string _allowAllCorsPolicyName = "AllowAll";
+
+        #endregion App Config Variables
+
+        public IWebHostEnvironment Env { get; }
+        protected IConfiguration Configuration { get; set; }
+
+        public Startup(IWebHostEnvironment env)
+        {
+            Env = env;
+            Configuration = GetBuilder().Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        public virtual IConfigurationBuilder GetBuilder()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Env.ContentRootPath)
+                .AddJsonFile("appsettings.json",
+                    optional: false,
+                    reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Env.EnvironmentName}.json",
+                    optional: true)
+                .AddEnvironmentVariables();
+
+            return config;
+        }
+    }
+}
