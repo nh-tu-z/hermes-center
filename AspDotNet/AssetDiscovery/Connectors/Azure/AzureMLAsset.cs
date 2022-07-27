0f9f8ff00ac81dd97b833f2d46c048d81bbe1771
Tu Ngo <nhtu1611@gmail.com>
2022-07-27 16:32:03 +0700

    init azure assets

---
+++ AspDotNet/AssetDiscovery/Connectors/Azure/AzureMLAsset.cs
@@ -0,0 +1,16 @@
+﻿namespace HermesCenter.AssetDiscovery.Azure
+{
+    public class AzureMLAsset : MLAsset
+    {
+        public static IList<MLAsset> EmptyList(string type, string parentId)
+        {
+            return new List<MLAsset>() {
+                new AzureMLAsset() { AssetType = type, ParentId = parentId, Id = Guid.Empty.ToString() }
+            };
+        }
+
+        public string SubscriptionId { get; set; }
+        public string ResourceGroupId { get; set; }
+        public string WorkspaceId { get; set; }
+    }
+}
---
+++ AspDotNet/AssetDiscovery/Connectors/Azure/AzureMLAssetFactory.cs
@@ -0,0 +1,24 @@
+﻿using HermesCenter.AssetDiscovery.Azure.Models;
+
+namespace HermesCenter.AssetDiscovery.Azure
+{
+
+    /// <summary>
+    /// TODO - add description
+    /// </summary>
+    public static class AzureMLAssetFactory
+    {
+        public static AzureMLAsset CreateSubscription(Subscription sub)
+        {
+            return new AzureMLAsset()
+            {
+                SubscriptionId = sub.SubscriptionId,
+                AssetType = AssetType.Subscription,
+                AssetName = sub.SubcriptionName,
+                AssetId = sub.SubscriptionId,
+                Data = sub,
+                IsHierarchy = true
+            };
+        }
+    }
+}
